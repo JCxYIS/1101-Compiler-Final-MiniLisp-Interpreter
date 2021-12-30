d5dfe35bc000e565b02a9804a4e196d4e4ee4870
JCxYIS@sf314 <jsn811@gmail.com>
2021-12-30 14:28:05 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1101_Compiler_Final___MiniLisp_Interpreter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
